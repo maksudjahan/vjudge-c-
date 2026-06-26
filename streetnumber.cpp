3d168a9e739e2818485c97a3f1b5c5e23802833f
Maksud Jahan <maksudjahan7@gmail.com>
2026-06-26 22:00:27 +0600

    Add files via upload

---
+++ streetnumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m,street;
+    cin>>n>>m;
+    if((n%2==0)&&(m%2==0))
+    cout<<1<<endl;
+    else if((n%2==1)&&(m%2==1))
+    cout<<1<<endl;
+    else
+        cout<<0<<endl;
+}
+
+
+
